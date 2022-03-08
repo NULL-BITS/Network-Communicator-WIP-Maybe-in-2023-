8036c210412621ec3b253a446238ba6bf5601a9b
TFang <80842080+TomFang1@users.noreply.github.com>
2022-03-08 22:14:15 +0100

    fixes
    fixed an issue

--- Network-communicator-client.py
+++ Network-communicator-client.py
@@ -4,22 +4,20 @@ PORT = ""
 
 def address():
     global HOST
-    print("What is the IP of the computer you want to ceonnect to? ")
+    print("What is the IP of the computer you want to connect to? ")
     HOST = input(":")
     global PORT
-    print("What is the PORT of the computer you want to ceonnect to? ")
+    print("What is the PORT of the computer you want to connect to? ")
     PORT = int(input(":"))
     connector()
-
 
 def connector():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall("Hello, world")
+        s.sendall(b"test")
         data = s.recv(1024)
-
-    print("Received {data!r}")
-
+    print(f"Received {data!r}")
+    
 address()
 
 
