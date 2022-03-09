c47d8e175e56178612a63ce3a4fdcc610b1805dc
Tom Fangmeyer <tom01@fangmeyer.org>
2022-03-09 09:17:38 +0100

    some updates

--- Network-communicator-client.py
+++ Network-communicator-client.py
@@ -1,6 +1,7 @@
 import socket
 HOST = ""
 PORT = ""  
+file = ""
 
 def address():
     global HOST
@@ -9,12 +10,16 @@ def address():
     global PORT
     print("What is the PORT of the computer you want to connect to? ")
     PORT = int(input(":"))
+    print("file do you want to send? ")
+    global file
+    file = input(":")
     connector()
 
 def connector():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(b"test")
+        global file
+        s.sendall(file)
         data = s.recv(1024)
     print(f"Received {data!r}")
     
--- Network-communicator-server.py
+++ Network-communicator-server.py
@@ -3,14 +3,22 @@ import socket
 HOST = "127.0.0.1"
 PORT = 65432
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as xsocket:
-    xsocket.bind((HOST, PORT))
-    xsocket.listen()
-    conn, addr = xsocket.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+def IP():
+    ip = socket.gethostbyname("localhost")
+    print("Your IP is: " + ip)
+    listener()
+
+def listener():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as xsocket:
+        xsocket.bind((HOST, PORT))
+        xsocket.listen()
+        conn, addr = xsocket.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                print(data.decode());
+                if not data:
+                    break
+                conn.sendall(data)
+IP()
